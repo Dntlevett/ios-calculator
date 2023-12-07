8a707bdcf7335f6da4a65095f0e8bf5706e72823
David Levett <dntlevett@gmail.comgit config user.namegit config user.emaildntlevett@gmail.com>
2023-12-07 13:39:30 -0800

    add function for calculating and click handlers for buttons

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "../src/app.scss";
+import React, { useState } from "react";
+
+function Calculator() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
+
+  const clear = () => {
+    setResult("");
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch (err) {
+      setResult("Error");
+    }
+  };
+  return <></>;
+}
+
+export default Calculator;
